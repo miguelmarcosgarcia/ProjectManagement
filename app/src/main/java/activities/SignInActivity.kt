f0617707e5ce513c37d172043ed3fd0cc87dad1c
Miguel Ángel Marcos <miguelangelmarcosgarcia@gmail.com>
2020-05-25 00:00:28 +0200

    FirebaseAuth: SignIn with email and password implemented

--- app/src/main/java/activities/SignInActivity.kt
+++ app/src/main/java/activities/SignInActivity.kt
@@ -1,37 +1,110 @@
 package activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.projectmanagement.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
-class SignInActivity : AppCompatActivity() {
-
+class SignInActivity : BaseActivity() {
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
         setContentView(R.layout.activity_sign_in)
-        setupActionBar()
 
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        setupActionBar()
+
+        // Add click event for sign-in button and call the function to sign in.)
+        btn_sign_in.setOnClickListener {
+            signInRegisteredUser()
+        }
+
     }
 
-    //SETTING UP THE ACTION BAR FUNCTION
-    private fun setupActionBar(){
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
         setSupportActionBar(toolbar_sign_in_activity)
 
         val actionBar = supportActionBar
-        if(actionBar!=null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
-            //New vector asset with the style of an iOS back button
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
-        //Adding functionality to the back arrow
-        toolbar_sign_in_activity.setNavigationOnClickListener{
-            onBackPressed()
+
+        toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    // TODO (Step 2: A function for Sign-In using the registered user using the email and password.)
+    // START
+    /**
+     * A function for Sign-In using the registered user using the email and password.
+     */
+    private fun signInRegisteredUser() {
+        // Here we get the text from editText and trim the space
+        val email: String = et_email_sign_in.text.toString().trim { it <= ' ' }
+        val password: String = et_password_sign_in.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Sign-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(
+                            this@SignInActivity,
+                            "You have successfully signed in.",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this@SignInActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
         }
+    }
+
+    /**
+     * A function to validate the entries of a user.
+     */
+    private  fun validateForm(email: String, password: String) : Boolean{
+        return when{
+            TextUtils.isEmpty(email) ->{
+                showErrorSnackBar("Please enter a email address")
+                false
+            }
+            TextUtils.isEmpty(password) ->{
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+
     }
 }
--- app/src/main/java/activities/SignUpActivity.kt
+++ app/src/main/java/activities/SignUpActivity.kt
@@ -1,6 +1,5 @@
 package activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.WindowManager
